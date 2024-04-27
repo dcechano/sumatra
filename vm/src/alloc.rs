bb0e443471e1b1583328afb704ecae4989bd77ed
dcechano <dcechano@emich.edu>
2024-04-26 22:09:45 -0400

    change module name

--- vm/src/alloc.rs
+++ vm/src/alloc.rs
@@ -6,7 +6,7 @@ pub mod alloc_type;
 pub mod fields_table;
 pub mod header;
 pub mod oop;
-pub mod static_alloc;
+pub mod static_fields;
 
 const VALUE_SIZE: usize = mem::size_of::<Value>();
 const VALUE_ALIGN: usize = mem::align_of::<Value>();
