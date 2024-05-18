33a3eb9697b096016a1af7a460ae7af7add5250e
dcechano <dcechano@emich.edu>
2024-05-18 18:07:14 -0400

    update api

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(warnings)] // TODO Remove
-
-use sumatra_parser::class_file::ClassFile;
-use sumatra_vm::{class::Class, vm::VM};
+use sumatra_vm::vm::VM;
 
 const CLASS_PATH: &str =
     "/home/dylan/Documents/RustProjects/sumatra/java/out/production/sumatra/files/";
+
+const JDK: &str = "/home/dylan/Documents/RustProjects/sumatra/jdk/compiled/java.base/";
 
 const CLASSES: [&str; 5] = [
     "/home/dylan/Documents/RustProjects/sumatra/java/out/production/sumatra/files/Main.class",
@@ -13,18 +13,8 @@ const CLASSES: [&str; 5] = [
     "/home/dylan/Documents/RustProjects/sumatra/java/out/production/sumatra/files/Simple.class",
     "/home/dylan/Documents/RustProjects/sumatra/parser/tests/rt/java/lang/System.class",
 ];
-fn main() {
-    let class = CLASSES[0];
-    let class_file = ClassFile::parse_class(class).unwrap();
-    // println!("Running {:#?}", class);
-    // println!("class {:#?}", class_file);
-    let mut vm = VM::init(CLASS_PATH.into());
-    vm.run("Simple.class").unwrap()
 
-    // for class in CLASSES {
-    //     println!();
-    //     println!("Parsing: {class}");
-    //     println!();
-    //     println!("{:#?}", ClassFile::parse_class(class).unwrap());
-    // }
+fn main() {
+    let mut vm = VM::init(JDK.into(), CLASS_PATH.into());
+    vm.run("Simple.class").unwrap()
 }
