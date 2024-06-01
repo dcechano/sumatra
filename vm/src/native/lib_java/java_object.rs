5922ad6f8af14a4f6a345619b5d446eb41368cba
dcechano <dcechano@emich.edu>
2024-06-01 04:46:20 -0400

    add native module for Object.java

---
+++ vm/src/native/lib_java/java_object.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+
+use crate::{
+    data_types::{reference_types::ObjRef, value::Value},
+    native::{lib_java::JAVA_LANG_OBJECT, native_identifier::NativeIdentifier},
+    vm::VM,
+};
+
+pub fn jvm_register_natives(
+    vm: &mut VM,
+    _: Option<ObjRef>,
+    _: Vec<Value>,
+) -> Result<Option<Value>> {
+    let native_identifier = NativeIdentifier::new(
+        JAVA_LANG_OBJECT.to_string(),
+        "getClass()Ljava/lang/Class".to_string(),
+    );
+    vm.native_registry
+        .register(native_identifier, jvm_get_class);
+    Ok(None)
+}
+
+fn jvm_get_class(
+    vm: &mut VM,
+    this: Option<ObjRef>,
+    _: Vec<Value>,
+) -> Result<Option<Value>> {
+    let class_obj = vm.get_class_obj(this.unwrap()).unwrap();
+    Ok(Some(Value::new_object(class_obj)))
+}
