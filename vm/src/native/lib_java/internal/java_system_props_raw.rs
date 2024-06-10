751981a79e5a33e6584eb9b41df6d2d7a729ea0c
dcechano <dcechano@emich.edu>
2024-06-10 18:34:23 -0400

    impl jvm_platform_properties native

--- vm/src/native/lib_java/internal/java_system_props_raw.rs
+++ vm/src/native/lib_java/internal/java_system_props_raw.rs
@@ -3,6 +3,7 @@ use crate::{
     vm::VM,
 };
 use anyhow::Result;
+use sumatra_parser::instruction::ArrayType;
 
 // const NATIVES: [(&str, NativeMethod); 1] = [(
 //     "platformProperties()[Ljava/lang/String;",
@@ -13,8 +14,12 @@ pub(crate) const PLATFORM_PROPS_SIG: &str = "platformProperties()[Ljava/lang/Str
 
 pub fn jvm_platform_properties(
     vm: &mut VM,
-    this: Option<ObjRef>,
+    _: Option<ObjRef>,
     _: Vec<Value>,
 ) -> Result<Option<Value>> {
-    todo!()
+    let mut string_array = vm.heap().new_array(2, ArrayType::Ref);
+    let key = vm.create_java_string("_display_country_NDX", false);
+    string_array.insert(0, Value::new_object(key));
+    string_array.insert(1, Value::Null);
+    Ok(Some(Value::new_array(string_array)))
 }
