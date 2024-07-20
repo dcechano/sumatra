f17faeff826ce3357bd587a221acb851eadb6af9
dcechano <dcechano@emich.edu>
2024-07-20 04:44:54 -0400

    add InitReqArray enum variant

--- vm/src/lli/response.rs
+++ vm/src/lli/response.rs
@@ -2,6 +2,9 @@ use crate::class::Class;
 
 pub(crate) enum Response {
     InitReq(&'static Class, usize),
+    /// InitReqArray(ArrayClass, index of array_class, Component class with its
+    /// index (if applicable))
+    InitReqArray(&'static Class, usize, Option<(&'static Class, usize)>),
     NotFound,
     Ready(usize),
 }
